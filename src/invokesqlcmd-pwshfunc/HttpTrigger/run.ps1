15d5db92ede4e65277e0c7d0399f5dc9266c0a29
John Downs <john.downs@microsoft.com>
2020-02-10 09:42:53 +1300

    Add function app example

---
+++ src/invokesqlcmd-pwshfunc/HttpTrigger/run.ps1
@@ -0,0 +1,16 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+# Write to the Azure Functions log stream.
+Write-Host "PowerShell HTTP trigger function processed a request."
+
+$connectionString = $env:SqlConnectionString
+Invoke-Sqlcmd -ConnectionString $connectionString -Query "SELECT COUNT(*) AS Count FROM MyTable"
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = [HttpStatusCode]::OK
+    Body = 'x'
+})
